4aab07971291c102e96b651b5b6a4bce545394e5
대원 김 <dwkim@anylogic.co.kr>
2025-01-28 20:48:19 +0900

    로그 트레이스 추가

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use axum::{
     Extension, Json, Router,
 };
 use dotenv::dotenv;
-use std::env;
+use std::{env, io};
 
 mod db;
 mod error;
@@ -14,16 +14,42 @@ mod models;
 mod routes; // 모듈 임포트
 
 use db::create_pool;
-use handlers::*;
+//use handlers::*;
+
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+use tracing_appender::rolling;
+use tracing_subscriber::{
+    fmt::writer::MakeWriterExt,
+    // layer::{self, SubscriberExt},
+    // util::SubscriberInitExt,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     dotenv().ok();
     let pool = create_pool().await?;
 
-    let app = routes::create_routes().layer(Extension(pool)); // 라우트를 앱에 추가
+    let info_file = rolling::daily("./logs", "info").with_max_level(tracing::Level::INFO);
+
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .pretty()
+        .json()
+        // .with_writer(io::stdout)
+        .with_max_level(tracing::Level::TRACE)
+        .with_writer(info_file)
+        .with_ansi(false)
+        .init();
+
+    let app = routes::create_routes().layer(Extension(pool)).layer(
+        TraceLayer::new_for_http()
+            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+    ); // 라우트를 앱에 추가
 
     println!("서버가 0.0.0.0:3000에서 시작됩니다...");
+    tracing::info!("listening on {}", "locahost:3000");
 
     let listener = tokio::net::TcpListener::bind("localhost:3000")
         .await
